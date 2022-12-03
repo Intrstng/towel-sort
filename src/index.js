eeb4c91ea34224d1025198e6a2c58155b0720666
Intrstng <Intrstng@gmail.com>
2022-12-03 20:01:26 +0300

    fix: add check for undefined input, task completed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+
+if (matrix === undefined || matrix.length == 0) return [];
+else {
+    return matrix.reduce((newArr, current, index) => {
+        let  arr2 = (index % 2 === 0) ? current : [...current].reverse();
+        return [...newArr, ...arr2];      
+       }, []);
+      };
 }
